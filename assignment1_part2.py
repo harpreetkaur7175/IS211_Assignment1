540bd2068d660f9502a53d946722bb3a3f5d370c
harpreetkaur7175 <89755899+harpreetkaur7175@users.noreply.github.com>
2021-09-09 21:54:53 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,15 @@
 
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    #only class (static) variables get defined here
+    #these are shared by all instance variables
+    def __init__(self, author="",title=""):
+        #instance variables get defined here
+        self.author = author
+        self.title = title
     def display(self):
-        pass
+        print("'"+self.title+"' by "+self.author)
 
-
-if __name__ == "__main__":
-    pass
+obj1 = Book("John Steinbeck","Of Mice and Men")
+obj2 = Book("Harper Lee","To kill a Mockingbird")
+obj1.display()
+obj2.display()
